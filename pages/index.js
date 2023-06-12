622a0192fbbb4bb5b9382c359dcd660391db78ad
Noskov Sergey <noskov.sergey@mail.ru>
2023-06-12 21:10:24 +0500

    start with cards. add like and auto create

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,35 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
 let redact = document.querySelector('.profile__redact_btn');
 let popup = document.querySelector('.popup');
 let popopclose = document.querySelector('.popup__btn_action_close')
+let likebutton = document.querySelector('.card__like_btn')
+let cardsContainer = document.querySelector('.elements__list')
 
 function OpenPopup() {
     popup.classList.add('popup_opened');
@@ -12,7 +41,9 @@ function ClosePopup() {
 
 redact.addEventListener('click', OpenPopup);
 popopclose.addEventListener('click', ClosePopup);
-///////////////////////////////////////////////////
+likebutton.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__like_btn_active');
+});
 
 let formElement = document.querySelector('.popup__form')
 
@@ -28,3 +59,20 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+function addCard(imgValue, paragraphValue) {
+    const cardTemplate = document.querySelector('#cards_template').content;
+    console.log(cardTemplate)
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+    console.log(cardElement)
+    cardElement.querySelector('.card__image').src = imgValue
+    cardElement.querySelector('.card__paragraph').textContent = paragraphValue
+    cardElement.querySelector('.card__like_btn').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('card_like_btn_active');
+    });
+    cardsContainer.append(cardElement);
+}
+
+initialCards.forEach(function(item) {
+    addCard(item.link, item.name);
+});
